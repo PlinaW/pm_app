276c7b62d304e9b9631f69037170e21ab2ccccfa
PlinaW <plinski.wojciech@gmail.com>
2023-05-25 17:48:39 +0200

    Updated associations in SprintBacklog model

--- app/models/sprint_backlog.rb
+++ app/models/sprint_backlog.rb
@@ -1,4 +1,6 @@
 class SprintBacklog < ApplicationRecord
   belongs_to :product_backlog
   has_many :features, through: :project_backlog
+  has_many :user_stories, through: :features
+  has_many :project_members, through: :project_backlog
 end
