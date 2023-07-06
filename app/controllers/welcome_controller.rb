ef1d01e50651bfe9d25843b419b15b4bd18643b1
PlinaW <plinski.wojciech@gmail.com>
2023-07-06 22:07:54 +0200

    Added new folder for devise layout

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
-  layout 'devise'
+  layout 'layouts/devise/devise'
   skip_before_action :authenticate_user!, only: [:index]
   before_action :redirect_logged_in_users
 
