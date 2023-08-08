911cce15e3b1e7f2c301a008c8eb6037feab1334
PlinaW <plinski.wojciech@gmail.com>
2023-08-08 21:16:45 +0200

    Update epics and projects controllers

--- app/controllers/epics_controller.rb
+++ app/controllers/epics_controller.rb
@@ -1,15 +1,48 @@
 class EpicsController < ApplicationController
-  def index; end
+
+  before_action :set_epic, only: %i[show edit update destroy]
+
+  def index
+    @epics = Epic.paginate(page: params[:page], per_page: 5)
+  end
 
   def show; end
 
-  def new; end
+  def new
+    @epic = Epic.new
+  end
 
-  def create; end
+  def create
+    @epic = Epic.new(epic_params)
+    if @epic.save
+      redirect_to @epic
+    else
+      render :new
+    end
+  end
 
   def edit; end
 
-  def update; end
+  def update
+    if @epic.update(epic_params)
+      redirect_to @epic
+    else
+      render :edit
+    end
+  end
 
-  def destroy; end
+  def destroy
+    @epic.destroy
+    redirect_to epics_path
+  end
+
+  private
+
+  def set_epic
+    @epic = Epic.find(params[:id])
+  end
+
+  def epic_params
+    params.require(:epic).permit
+  end
 end
