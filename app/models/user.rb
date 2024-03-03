eca2777caf4f749aae5bee15da5ee16917eed55f
PlinaW <plinski.wojciech@gmail.com>
2024-03-03 19:02:41 +0100

    Add role column to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,8 @@ class User < ApplicationRecord
   has_many :authored_epics, class_name: 'Epic', foreign_key: 'author_id'
   has_many :tasks, dependent: :nullify
 
+  enum role: %i[user admin]
+
   # validates :first_name, :last_name, presence: true
 
   def full_name
