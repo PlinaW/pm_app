8145c80c363a1cfd3d5e73d24b5fdf6014860230
PlinaW <110122241+PlinaW@users.noreply.github.com>
2024-03-07 18:12:45 +0100

    ProjectUsers update (#25)
    * Add index action to ProjectUsersController
    
    * ProjectUsers index view updated

--- app/controllers/project_users_controller.rb
+++ app/controllers/project_users_controller.rb
@@ -2,22 +2,26 @@ class ProjectUsersController < ApplicationController
 
   before_action :set_project
 
+  def index
+    @project_users = User.joins(:project_users).where(project_users: { project_id: @project })
+  end
+
   def invite
     email = params[:email]
-    return redirect_to project_path(@project), alert: 'No email provided' if email.blank?
+    return redirect_to project_project_users_path(@project), alert: 'No email provided' if email.blank?
 
     user = User.find_by(email:) || User.invite!({ email: }, current_user)
-    return redirect_to project_path(@project), alert: 'Email invalid' unless user.valid?
+    return redirect_to project_project_users_path(@project), alert: 'Email invalid' unless user.valid?
 
     user.project_users.find_or_create_by(project: @project, role: 0)
 
-    redirect_to project_path(@project), notice: "#{email} was invited"
+    redirect_to project_project_users_path(@project), notice: "#{email} was invited"
   end
 
   def destroy
     @project_user = @project.project_users.find(params[:id])
     @project_user.destroy
-    redirect_to project_path(@project), notice: 'User removed from project'
+    redirect_to project_project_users_path(@project), notice: 'User removed from project'
   end
 
 end
