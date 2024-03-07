fc788bcf7f2c0d0ef0c241d8eb77befd3b0a48a3
PlinaW <plinski.wojciech@gmail.com>
2024-03-07 18:49:04 +0100

    Add name validation

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,7 @@ class Team < ApplicationRecord
   belongs_to :project
   has_many :team_users, dependent: :destroy
   has_many :users, through: :team_users
+
+  validates :name, presence: true
+
 end
